aac43a331eaa6e187e30cb4b01e5008d10719553
funnellw <will.funnell@lbi.com>
2013-11-29 15:21:51 +0000

    initial commit

---
+++ project.clj
@@ -0,0 +1,17 @@
+(defproject twit "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [org.clojure/clojurescript "0.0-2080"]]
+  :plugins [[lein-cljsbuild "1.0.1-SNAPSHOT"]]
+  :source-paths ["src/clj"]
+  :cljsbuild {
+     :builds [{
+     :source-path "src/cljs"
+     :compiler {
+         :output-to "resources/public/twit.js"
+         :optimizations :whitespace
+         :pretty-print true}}]}
+  )
---
+++ test/clj/twit/core_test.clj
@@ -0,0 +1,7 @@
+(ns twit.core-test
+  (:use clojure.test
+        twit.core))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
